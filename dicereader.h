c1b12f90d422ac757b99f99f96624a99c8481f30
Michael Taylor <90michaeltaylor@gmail.com>
2015-07-28 14:18:21 -0700

    starting project version

---
+++ dicereader.h
@@ -0,0 +1,26 @@
+#ifndef DICEREADER_H
+#define DICEREADER_H
+
+#include<SFML/Graphics.hpp>
+
+#include<string>
+#include<iostream>
+using namespace std;
+
+class DiceReader {
+  public;
+
+  DiceReader();
+
+  unsigned int diceCount(string imageName);	
+
+  private;
+
+  sf::Image Image;
+  sf::Vector2u imageSize;
+  
+  unsigned int diceTotal;
+  void floodFind();
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<SFML/Graphics.hpp>
+
+using namespace std;
+
+int main()
+{
+  sf::Image Image;
+  if(!Image.loadFromFile("dice.png"))
+    return 1;
+
+  sf::Vector2u image_size;
+
+  image_size = Image.getSize();
+
+  cout << "X=" << image_size.x << endl;
+  cout << "Y=" << image_size.y << endl;
+
+  cout << "hello" << endl;
+  return 0;
+}
